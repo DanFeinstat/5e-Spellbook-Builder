e9351d0469c70fb71e210edaedf8bfa17c1d0f13
DanFein <danfeinstat@gmail.com>
2018-12-02 14:03:27 -0800

    updated server

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(bodyParser.json());
 
 // app.use(express.static(path.join(__dirname, "client/build")));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
