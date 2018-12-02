487464dd69b243c0628d8e66c2e2950d61733abc
DanFein <danfeinstat@gmail.com>
2018-12-02 14:50:30 -0800

    it's working?

--- controller/spellsController.js
+++ controller/spellsController.js
@@ -1,34 +1,38 @@
 const db = require("../models");
-const spellData = require("../spells.json");
-var express = require("express");
-var app = express();
+// const spellData = require("../spells.json");
+// var express = require("express");
+// var app = express();
 
 module.exports = {
-  findAll: (req, res) => {
-    res.json(spellData);
-  },
-
   //   findAll: (req, res) => {
-  //     db.Spells.find()
-  //       .then(dbModel => {
-  //         console.log(dbModel);
-  //         res.json(dbModel);
-  //       })
-  //       .catch(err => res.status(422).json(err));
+  //     res.json(spellData);
   //   },
-  //   findClassList: (req, res) => {
-  //     db.Spells.aggregate([
-  //       { $unwind: "$data" },
-  //       {
-  //         $match: {
-  //           "data.class": { $regex: req.params.classname },
-  //         },
-  //       },
-  //     ])
-  //       .then(dbModel => {
-  //         console.log(dbModel);
-  //         res.json(dbModel);
-  //       })
-  //       .catch(err => res.status(422).json(err));
-  //   },
+
+  //   findClassList:(req, res) => {
+
+  //   }
+
+  findAll: (req, res) => {
+    db.Spells.find()
+      .then(dbModel => {
+        console.log(dbModel);
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  findClassList: (req, res) => {
+    db.Spells.aggregate([
+      { $unwind: "$data" },
+      {
+        $match: {
+          "data.class": { $regex: req.params.classname },
+        },
+      },
+    ])
+      .then(dbModel => {
+        console.log(dbModel);
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
+  },
 };
