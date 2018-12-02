c17c43d83b949dbb5b9422c1063a79740183a7c6
DanFein <danfeinstat@gmail.com>
2018-12-02 14:14:43 -0800

    attempt 1billion

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
+  const path = require("path");
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
