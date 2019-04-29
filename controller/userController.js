46cc9b310f2c4c5306f3cf94e32e59c37fc7d767
DanFein <danfeinstat@gmail.com>
2019-04-29 15:25:24 -0700

    fixed auth error with the password hash comparison and updated failed login attempts with an alert on error

--- controller/userController.js
+++ controller/userController.js
@@ -9,16 +9,23 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Users.create(req.body)
-      .then(user => {
-        const spellbookJwt = jwt.sign(
-          { _id: req.body._id },
-          process.env.SECRET
-        );
-
-        res.status(200).send({ userID: user.id, spellbookJwt }); //probably going to need to add a route for finding a specific user
-      })
+    db.Users.create({
+      names: req.body.names,
+      email: req.body.email,
+      password: req.body.password,
+      spellbooks: req.body.spellbooks,
+    })
+      .then(user => res.json(user))
       .catch(err => res.status(422).json(err));
+    // .then(user => {
+    //   const spellbookJwt = jwt.sign(
+    //     { _id: req.body._id },
+    //     process.env.SECRET
+    //   );
+
+    //   res.status(200).send({ userID: user.id, spellbookJwt }); //probably going to need to add a route for finding a specific user
+    // })
+    // .catch(err => res.status(422).json(err));
   },
 
   getUser: (req, res) => {
@@ -84,24 +91,49 @@ module.exports = {
       .then(user => res.status(200).send(user))
       .catch(err => res.status(404).send(err));
   },
-  login: function(req, res) {
-    db.Users.findOne({ email: req.body.email })
-      .then(dbModel => {
-        var passwordResult = bcrypt.compare(
-          req.body.password,
-          dbModel.password
-        );
+  //   login: function(req, res) {
+  //     db.Users.findOne({ email: req.body.email })
+  //       .then(dbModel => {
+  //         var passwordResult = bcrypt.compare(
+  //           req.body.password,
+  //           dbModel.password
+  //         );
 
-        if (passwordResult) {
+  //         if (passwordResult) {
+  //           const spellbookJwt = jwt.sign(
+  //             { id: dbModel._id },
+  //             process.env.SECRET
+  //           );
+  //           res.status(200).send({ spellbookJwt, dbModel });
+  //         } else {
+  //           res.status(404).send({ message: "Incorrect Password" });
+  //         }
+  //       })
+  //       .catch(err => res.status(422).json({ message: "Still not working" }));
+  //   },
+  // };
+
+  login: function(req, res, next) {
+    db.Users.findOne({ email: req.body.email }, function(err, userInfo) {
+      if (err) {
+        // next(err);
+        res.status(404).send({ message: "Invalid email" });
+        // res.json({ status: `error`, message: `Invalid email`, data: null });
+      } else {
+        if (bcrypt.compareSync(req.body.password, userInfo.password)) {
           const spellbookJwt = jwt.sign(
-            { id: dbModel._id },
+            { id: userInfo._id },
             process.env.SECRET
           );
-          res.status(200).send({ spellbookJwt, dbModel });
+          res.status(200).send({ spellbookJwt, userInfo });
         } else {
-          res.status(404).send({ message: "Incorrect Password" });
+          res.status(404).send({
+            message: "Incorrect Password",
+          });
         }
-      })
-      .catch(err => res.status(422).json({ message: "Still not working" }));
+      }
+    }).catch(err =>
+      res.status(422).json({ message: "Email or password invalid" })
+    );
   },
 };
