ed7a672c0a884c1c549d762d3eb63e90e90495fe
DanFein <danfeinstat@gmail.com>
2019-05-04 15:24:19 -0700

    update card and spell list animations

--- client/src/components/Search/SearchList.js
+++ client/src/components/Search/SearchList.js
@@ -1,54 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
+import { Spring } from "react-spring/renderprops";
+
 import styles from "./SearchList.module.css";
 
-const SearchList = props => {
-  return (
-    <div className={styles.container}>
-      {props.classSelected != null
-        ? props.listofLists.map((level, index) => {
-            let listNumber = index;
-            let spellLevel =
-              props.classSelected !== "Paladin" &&
-              props.classSelected !== "Ranger"
-                ? index === 0
-                  ? "Cantrip"
-                  : index
-                : index + 1;
-            let spellList = "level" + listNumber;
-            return level.length ? (
-              <React.Fragment>
-                <h4
-                  className={styles.level}
-                  onClick={props.toggleList}
-                  data-level={"level" + listNumber}
-                >
-                  Spell Level: {spellLevel}
-                </h4>
-                <ul key={listNumber}>
-                  {level.map((list, index) => {
-                    const toggleKey = "level" + listNumber;
-                    return (
-                      <li
-                        className={
-                          props[toggleKey] === true
-                            ? styles.li
-                            : `${styles.li} ${styles.hidden}`
-                        }
-                        key={index}
-                        data-name={list.data.name}
-                        onClick={props.fetchSpell}
-                      >
-                        {list.data.name}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </React.Fragment>
-            ) : null;
-          })
-        : null}
-    </div>
-  );
-};
+class SearchList extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        {this.props.classSelected != null
+          ? this.props.listofLists.map((level, index) => {
+              let listNumber = index;
+              let spellLevel =
+                this.props.classSelected !== "Paladin" &&
+                this.props.classSelected !== "Ranger"
+                  ? index === 0
+                    ? "Cantrip"
+                    : index
+                  : index + 1;
+              let spellList = "level" + listNumber;
+              return level.length ? (
+                <React.Fragment>
+                  <h4
+                    className={styles.level}
+                    onClick={this.props.toggleList}
+                    data-level={"level" + listNumber}
+                  >
+                    Spell Level: {spellLevel}
+                  </h4>
+                  <ul key={listNumber}>
+                    {this.props[`level${listNumber}`]
+                      ? level.map((list, index) => {
+                          // const toggleKey = "level" + listNumber;
+                          return (
+                            <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+                              {props => (
+                                <li
+                                  className={
+                                    // props[toggleKey] === true
+                                    // ?
+                                    styles.li
+                                    // : `${styles.li} ${styles.hidden}`
+                                  }
+                                  style={props}
+                                  key={index}
+                                  data-name={list.data.name}
+                                  onClick={this.props.fetchSpell}
+                                >
+                                  {list.data.name}
+                                </li>
+                              )}
+                            </Spring>
+                          );
+                        })
+                      : null}
+                  </ul>
+                </React.Fragment>
+              ) : null;
+            })
+          : null}
+      </div>
+    );
+  }
+}
 
 export default SearchList;
